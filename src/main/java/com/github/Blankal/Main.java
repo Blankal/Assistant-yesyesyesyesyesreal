3e8d421409fe27425ac0660622d31101e99bbd1c
KhartDa <khart.david10@gmail.com>
2025-12-16 10:06:30 -0800

    please work

--- src/main/java/com/github/Blankal/Main.java
+++ src/main/java/com/github/Blankal/Main.java
@@ -3,11 +3,13 @@ package com.github.Blankal;
 import com.github.Blankal.GeminiAPI;  // Generate text based off text and/or image input (Google)
 import com.github.Blankal.OpenAI_API;  // Generate Text based off Text and/or image input (OpenAI)
 import com.github.Blankal.OmniRequest;  // Used to let the AI model see via a Json array
+import com.github.Blankal.Tools;
 import static com.github.Blankal.ScreenCapture.getFrame;  // Generates base64 screenshots to be analyzed by OmniParse
 import static com.github.Blankal.config.getModelType;
 import static com.github.Blankal.config.getUserPrompt;
 import static com.github.Blankal.config.getInstructions;
 import static com.github.Blankal.config.getModelBrand;
+import static com.github.Blankal.Tools;
 
 public class Main 
 {
@@ -20,6 +22,7 @@ public class Main
         try
         {
             OpenAI_API.generateStaticFeedback(payload);
+
             
             if(maxLoops > 0)
             {
